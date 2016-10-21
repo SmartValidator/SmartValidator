4b04e953dd7d8b309bcfad0f1e2becfd17b63537
RefiPeretz <refi11991@gmail.com>
2016-10-21 18:23:49 +0300

    Update xml Iana

--- rpki-validator-app/src/main/scala/net/ripe/rpki/validator/views/BlockListView.scala
+++ rpki-validator-app/src/main/scala/net/ripe/rpki/validator/views/BlockListView.scala
@@ -1,22 +1,122 @@
 package net.ripe.rpki.validator.views
 
-import net.ripe.rpki.validator.bgp.preview.BgpValidatedAnnouncement
+import net.ripe.rpki.validator.iana.block.IanaAnnouncement
 import net.ripe.rpki.validator.lib.Validation.FeedbackMessage
-import net.ripe.rpki.validator.models.BlockList
+import net.ripe.rpki.validator.models.{BlockList, RouteValidity}
 
-import scala.xml.Text
+import scala.xml.{Text, Xhtml}
 
 /**
   * Created by fimka on 14/10/16.
   */
-class BlockListView(blockList: BlockList, validatedAnnouncements: Seq[BgpValidatedAnnouncement], params: Map[String, String] = Map.empty, messages: Seq[FeedbackMessage] = Seq.empty) extends View with ViewHelpers {
-  private val fieldNameToText = Map("asn" -> "Origin", "prefix" -> "Prefix", "maxPrefixLength" -> "Maximum prefix length")
+class BlockListView(blockList: BlockList, validatedAnnouncements: Seq[IanaAnnouncement], params: Map[String, String] = Map.empty, messages: Seq[FeedbackMessage] = Seq.empty) extends View with ViewHelpers {
+  private val fieldNameToText = Map("prefix" -> "Prefix")
 //  val currentRtrPrefixes = getCurrentRtrPrefixes()
 
   def tab = Tabs.BlockListTab
-  def title = Text("Whitelist")
-  def body =
-  {
-    <div></div>
+  def title = Text("Blocklist")
+  def body = {
+    <div>{ renderMessages(messages, fieldNameToText) }</div>
+      <div class="alert-message block-message info" data-alert="alert">
+        <a class="close" href="#">×</a>
+        <p>
+          add info
+        </p>
+        <p>
+          add info
+        </p>
+      </div>
+      <h2>Add entry</h2>
+      <div class="well">
+        <form method="POST" class="form-stacked">
+          <fieldset>
+            <div>
+              <div class="span4"><label for="announcement-asn">Origin</label></div>
+              <div class="span4"></div>
+            </div>
+            <div class="span4">
+              <input id="announcement-asn" type="text" name="asn" value={ params.getOrElse("asn", "") } placeholder="ASN (required)"/>
+            </div>
+            <div class="span4">
+              <input id="announcement-prefix" type="text" name="prefix" value={ params.getOrElse("prefix", "") } placeholder="IPv4 or IPv6 prefix (required)"/>
+            </div>
+            <div class="span4">
+              <input id="announcement-maxprefixlen" type="text" name="maxPrefixLength" value={ params.getOrElse("maxPrefixLength", "") } placeholder="Number (optional)"/>
+            </div>
+            <div class="span2">
+              <input type="submit" class="btn primary" value="Add"/>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+      <div>
+        <h2>Current entries</h2>{
+        if (blockList.entries.isEmpty)
+          <div class="alert-message block-message"><p>No whitelist entries defined.</p></div>
+        else {
+          <table id="whitelist-table" class="zebra-striped" style="display: none;">
+            <thead>
+              <tr>
+               <th>Prefix</th><th>&nbsp;</th>
+              </tr>
+            </thead>
+            <tbody>{
+              for (entry <- blockList.entries) yield {
+
+
+                def makeDetailsTable(announcements: Seq[IanaAnnouncement]) = {
+                  <table>
+                    <thead>
+                      <tr><th>Prefix</th></tr>
+                    </thead>
+                    {
+                    for { announcement <- announcements } yield {
+                      <tr>
+                        <td> { announcement.prefix.toString } </td>
+                      </tr>
+                    }
+                    }
+                  </table>
+                }
+
+                <tr>
+                  <td>{ entry.prefix }</td>
+                  <td>
+                    <form method="POST" action="/blocklist" style="padding:0;margin:0;">
+                      <input type="hidden" name="_method" value="DELETE"/>
+                      <input type="hidden" name="prefix" value={ entry.prefix.toString }/>
+                      <input type="submit" class="btn" value="delete"/>
+                    </form>
+                  </td>
+                </tr>
+              }
+              }</tbody>
+          </table>
+            <script><!--
+$(document).ready(function() {
+  $('#whitelist-table').dataTable({
+      "sPaginationType": "full_numbers",
+      "aoColumns": [
+        { "sType": "numeric" },
+        null,
+        { "sType": "numeric" },
+        { "bSortable": false },
+        { "bSortable": false },
+        { "bSortable": false }
+      ]
+    }).show();
+  $('[rel=popover]').popover({
+    "live": true,
+    "html": true,
+    "placement": "below",
+    "offset": 10
+  }).live('click', function (e) {
+    e.preventDefault();
+  });
+});
+// --></script>
+        }
+        }
+      </div>
   }
 }
